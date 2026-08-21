0c40391a7f51079217aca1a7e3c8af40146b7823
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-21 15:54:34 +0530

    create String representation

--- inventory/models.py
+++ inventory/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     quantity = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
