3052e15a3b11ae38a29cb8d3831e11a0d97373b4
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-27 09:58:56 +0530

    add_product

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from inventory.models import Product
+from inventory.forms import ProductForm
+from .models import Product
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'inventory/product_list.html', {'products': products})
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list') 
+    else:
+        form = ProductForm()
+    
+    return render(request, 'inventory/add_product.html', {'form': form})
