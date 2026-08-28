58c21ff22d728dd884816858c80addba98471fe6
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-28 17:27:50 +0530

    create delete_product url

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('add/', views.add_product, name='add_product'),
     path('update/<int:pk>/', views.update_product, name='update_product'),
+    path('delete/<int:pk>/', views.delete_product, name='delete_product'),
 ]
