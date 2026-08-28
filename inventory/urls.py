22601bb8073f5ea20f38a3d0d9b455fb4740d60e
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-28 17:27:19 +0530

    create update_product url

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('add/', views.add_product, name='add_product'),
+    path('update/<int:pk>/', views.update_product, name='update_product'),
 ]
